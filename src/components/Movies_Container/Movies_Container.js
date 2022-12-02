40551ac0ee4a1f6487bcbb1df13eac6247ddca6c
Brett Kuhn <brettkuhn87@gmail.com>
2022-12-02 15:59:19 -0700

    Updated code to pass props to MovieDetail correctly and destructure it, along with the basic functions to show and exit movie

--- src/components/Movies_Container/Movies_Container.js
+++ src/components/Movies_Container/Movies_Container.js
@@ -1,28 +1,47 @@
 import React, { Component } from "react";
 import App from "../App/App";
 import Poster from "../Poster/Poster";
+import MovieDetail from "../Movie_Detail/Movie_Detail";
 
 
 class MoviesContainer extends Component {
   constructor() {
     super();
     this.state = {
-      selectedMovie: ''
+      selectedMovie: {
+        "id": 694919,
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "title": "Money Plane",
+        "average_rating": 6.666666666666667,
+        "release_date": "2020-09-29"
+      }
     }
   }
 
-  
+  showMovie = (event) => {
+    this.setState({})
+  }
+
+  exitMovie = (event) => {
+    this.setState({})
+  }
 
   render() {
-
     const moviePosters = this.props.movies
       .map(movie => {
-        return <Poster title={movie.title}/>
+        return <Poster 
+          title={movie.title}
+          poster={movie.poster_path}
+          id={movie.id}
+          />
       });
 
-
     return (
-      <h1>{moviePosters}</h1>
+      <>
+        {this.state.selectedMovie && <MovieDetail movie={this.state.selectedMovie}/>}
+        {!this.state.selectedMovie && <div>{moviePosters}</div>}
+      </>
     )
   }
 }
--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Poster = ({title}) => {
+const Poster = ({title, poster, id}) => {
   return (
-    <h1>{title}</h1>
+    <>
+    </>
   )
 }
 
