21e2e00bcdb635a455cde25ec297bf719048059f
Spencer Haka <spencer.haka@gmail.com>
2022-12-04 18:01:56 -0700

    Complete refactor of movie detail so that user can click on external link

--- src/components/Movie_Detail/Movie_Detail.js
+++ src/components/Movie_Detail/Movie_Detail.js
@@ -4,21 +4,30 @@ import './Movie_Detail.css'
 const MovieDetail = ({ movie, videos, exitMovie }) => {
 
   const background = { backgroundImage: `url(${movie.backdrop_path})` }
-  const trailer = `https://www.youtube.com/watch?v=${videos[0].key}`
-  console.log(videos[0].key)
+
+  const trailer = (videos) => {
+    if (videos[0].site === 'YouTube') {
+      return `https://www.youtube.com/watch?v=${videos[0].key}`
+    } else if (videos[0].site === 'Vimeo') {
+      return `https://www.vimeo.com/${videos[0].key}`
+    }
+  }
+
   return (
     <div className="movie-detail" style={background}>
       <img src={movie.poster_path} alt={`poster for ${movie.title}`} className='poster-detail'></img>
       <div className="details">
-        <p>Title: {movie.title}</p>
+        <h1>{movie.title}</h1>
         {!!movie.tagline && <p>{movie.tagline}</p>}
         <p>Rating: {movie.average_rating.toFixed(1)}</p>
         <p>Release Date: {movie.release_date}</p>
         {!!movie.budget && <p>Budget: {movie.budget}</p>}
         {!!movie.revenue && <p>Revenue: {movie.revenue}</p>}
-        <p>Runtime: {movie.runtime} minutes</p>
+        {!!movie.runtime && <p>Runtime: {movie.runtime} minutes</p>}
         <p>Overview: {movie.overview}</p>
-        <p>Watch the <a href={trailer} target="_blank" rel="noreferrer">trailer</a>!</p>
+        {!!videos.length && 
+        <p>Watch the <a href={trailer(videos)} target="_blank" rel="noreferrer">trailer</a>!</p>
+        }
         <button onClick={() => exitMovie()}>Return</button>
       </div>
     </div>
