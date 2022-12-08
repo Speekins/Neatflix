fe52b98e1d2317e5fd2bd7ff014ab478efb40c53
Spencer Haka <spencer.haka@gmail.com>
2022-12-08 15:46:19 -0700

    Add link to poster componenet

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -1,16 +1,19 @@
 import React from "react"
 import './Poster.css'
+import { Link } from "react-router-dom"
 
 const Poster = ({ title, poster, id, showMovie }) => {
   return (
-    <div id={id} className='poster-container'>
-      <img 
-        className='poster'
-        onClick={(event) => showMovie(event)}
-        src={poster}
-      />
-      <p onClick={(event) => showMovie(event)} className='poster-title'>{title}</p>
-    </div>
+    <Link to={`/${id}`}>
+      <div id={id} className='poster-container'>
+        <img
+          className='poster'
+          // onClick={(event) => showMovie(event)}
+          src={poster}
+        />
+        <p /*onClick={(event) => showMovie(event)}*/ className='poster-title'>{title}</p>
+      </div>
+    </Link>
   )
 }
 
