881120985dd3aecd31bcd9c41ea92762cf7ea770
Brett Kuhn <brettkuhn87@gmail.com>
2022-12-07 20:36:30 -0700

    Basic tests for movie data showing up

--- cypress/e2e/moviedetail.cy.js
+++ cypress/e2e/moviedetail.cy.js
@@ -25,10 +25,28 @@ describe('Specific movie detail', () => {
 
   it('Should show the name of the movie', () => {
     cy.contains('Money Plane')
-  })
+  });
 
-  it('Should show rating', () => {
+  it('Should show the rating', () => {
     cy.contains('Rating: 6.9')
-  })
+  });
+
+  it('Should show the release date', () => {
+    cy.contains('Release Date: 2020-09-29')
+  });
+
+  it('Should show the runtime', () => {
+    cy.contains('Runtime: 82 minutes')
+  });
+
+  it('Should show an overview', () => {
+    cy.contains(`Overview: A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.`)
+  });
+
+  it('Should have a link to the trailer', () => {
+    cy.contains('Runtime: 82 minutes')
+  });
+
+
 
 })
