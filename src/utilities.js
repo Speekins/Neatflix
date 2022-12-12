cc5dc638f60396b74a796b5be0aa3215e9614be0
Brett Kuhn <brettkuhn87@gmail.com>
2022-12-12 13:10:32 -0700

    Makes function to format the date

--- src/utilities.js
+++ src/utilities.js
@@ -6,15 +6,25 @@ const formatCurrency = (dataObj) => {
       currency: 'USD',
       maximumSignificantDigits: 3}).format(dataObj.budget)
   }
-
   if(dataObj.revenue) {
     dataObj.revenue = new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
       maximumSignificantDigits: 3}).format(dataObj.revenue)
   }
-
   return dataObj
 }
 
-  export default formatCurrency
+const formatDate = (dataObj) => {
+  if (dataObj.release_date) {
+    const releaseDateData = dataObj.release_date.split('-')
+    const date = new Date()
+    date.setMonth(+releaseDateData[1]-1)
+    const month = date.toLocaleString('en-US', { month: 'long' })
+
+    dataObj.release_date = `${month} ${+releaseDateData[2]}, ${releaseDateData[0]}`
+  }
+  return dataObj
+}
+
+  export { formatCurrency, formatDate }
