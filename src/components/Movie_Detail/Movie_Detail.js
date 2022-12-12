6e21f7f41ae90bfe354fede2217fb25d8c5c1dc0
Spencer Haka <spencer.haka@gmail.com>
2022-12-11 20:39:36 -0700

    Make all warning consistent with warning classname

--- src/components/Movie_Detail/Movie_Detail.js
+++ src/components/Movie_Detail/Movie_Detail.js
@@ -42,7 +42,7 @@ class MovieDetail extends Component {
 
     return (
       <>
-        {this.state.error && <h2>Something went wrong...({this.state.error.message})</h2>}
+        {this.state.error && <h1 className="warning">Something went wrong...({this.state.error.message})</h1>}
         {!this.state.error &&
           <div className="movie-detail" style={background}>
             <img src={movie.poster_path} alt={`poster for ${movie.title}`} className='poster-detail'></img>
